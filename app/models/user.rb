03367ea882ba4747d90fb28826e0797864501747
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-06 10:19:25 -0600

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :password_confirmation, presence: true
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates :password, length: { minimum: 8 }
+
+
 end
