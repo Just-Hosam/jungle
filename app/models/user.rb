f6e123d2196ce522b3b3879afc5123584e6c45db
Hosam Dahrooge <hosam_firas@hotmail.com>
2021-04-06 11:34:04 -0600

    authentication class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,16 @@ class User < ActiveRecord::Base
   
   has_secure_password
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    is_authenticated = user.authenticate(password)
+    (user && is_authenticated) ? user : nil
+  end
+
   validates :email, presence: true
   validates :name, presence: true
   validates :password_confirmation, presence: true
   validates_uniqueness_of :email, :case_sensitive => false
   validates :password, length: { minimum: 8 }
-
 
 end
